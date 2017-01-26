d345977084ac95c03146a67f078fe554835dd61a
oleh <tajcig@ya.ru>
2017-01-26 16:11:39 +0200

    added mobile resolution

--- src/main/java/com/test/Launcher.java
+++ src/main/java/com/test/Launcher.java
@@ -3,13 +3,16 @@ package com.test;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Launcher {
 
 
     public static void main(String[] args) {
-
-        LwjglApplication lwjglApplication = new LwjglApplication(new Applicatoin());
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        configuration.width = 480;
+        configuration.height = 800;
+        LwjglApplication lwjglApplication = new LwjglApplication(new Applicatoin(),configuration);
 
     }
 }

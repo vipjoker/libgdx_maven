46f6bafe2db8c83270044e7eea497294a66b1eb8
oleh <tajcig@ya.ru>
2017-02-01 09:17:13 +0200

    adedd menu

--- src/main/java/com/test/FirstScreen.java
+++ src/main/java/com/test/FirstScreen.java
@@ -3,13 +3,36 @@ package com.test;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 /**
  * Created by oleh on 31.01.17.
  */
 public class FirstScreen implements ApplicationListener {
-    public void create() {
 
+    Button btnPlay;
+    Table mStage;
+    SpriteBatch batch;
+
+    public void create() {
+        batch = new SpriteBatch();
+        mStage = new Table();
+        Skin skin = new Skin();
+        skin.add("btn_press",new Texture("screen/button_pressed.png"));
+        skin.add("btn_unpress",new Texture("screen/button_unpressed.png"));
+        Button.ButtonStyle style = new Button.ButtonStyle();
+        style.down = skin.getDrawable("btn_press");
+        style.up = skin.getDrawable("btn_unpress");
+        btnPlay = new Button(style);
+        mStage.setDebug(true);
+        mStage.row();
+        mStage.add(btnPlay).center();
+        mStage.row().expandY();
     }
 
     public void resize(int width, int height) {
@@ -17,8 +40,11 @@ public class FirstScreen implements ApplicationListener {
     }
 
     public void render() {
-        Gdx.gl20.glClearColor(255/14f,255/95f,118f,1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl20.glClearColor(14f/255f,95f/255f,118f/255f,1);
+        batch.begin();
+        mStage.draw(batch,1);
+        batch.end();
     }
 
     public void pause() {
